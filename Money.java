e54f22da3190107205ad78321c7118de4e2e431e
Matheus Rodrigues <matheusarp70@gmail.com>
2026-07-12 10:15:45 -0300

    Passo Currency: Adicionado atributo e construtores na classe pai

--- Money.java
+++ Money.java
@@ -1,16 +1,26 @@
 abstract class Money {
     protected int amount;
-    
-    static Money dollar(int amount) {
-        return new Dollar(amount);
+    protected String currency; 
+
+    Money(int amount, String currency) {
+        this.amount = amount;
+        this.currency = currency;
     }
-    
+
+    static Money dollar(int amount)  {
+        return new Dollar(amount, "USD");
+    }
+
     static Money franc(int amount) {
-        return new Franc(amount);
+        return new Franc(amount, "CHF");
     }
-    
-    abstract Money times(int multiplier);  
-    
+
+    String currency() {
+        return currency;
+    }
+
+    abstract Money times(int multiplier);
+
     @Override
     public boolean equals(Object object) {
         Money money = (Money) object;
